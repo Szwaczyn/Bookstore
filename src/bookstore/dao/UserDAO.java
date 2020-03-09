13e736d064df4d8c247ef3327f6c7d7192c883e6
ms <ms@WAWL2011.waw.sita.main>
2020-03-09 19:35:50 +0100

    Hashing method

--- src/bookstore/dao/UserDAO.java
+++ src/bookstore/dao/UserDAO.java
@@ -1,10 +1,13 @@
 package bookstore.dao;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
 import bookstore.entity.User;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
+import javax.xml.bind.DatatypeConverter;
 
 public class UserDAO {
 
@@ -14,7 +17,7 @@ public class UserDAO {
 		this.em = em;
 	}
 	
-	public List<User> getUsers() {
+	public List<User> getUser() {
 		List<User> users = this.em.createQuery("SELECT u FROM User u").getResultList();
 		return users;
 	}
@@ -22,6 +25,14 @@ public class UserDAO {
 	public User getUser(int id) {
 		this.em.clear();
 		return this.em.find(User.class, id);
+	}
+	
+	public User getUser(String login) {
+		User user = (User)em.createQuery("SELECT u FROM user u WHERE u.login = :login")
+				.setParameter("login", login)
+				.getSingleResult();
+		
+		return user;
 	}
 	
 	public boolean addUser(User u) {
@@ -36,6 +47,20 @@ public class UserDAO {
 			e.printStackTrace();
 			et.rollback();
 			return false;
+		}
+	}
+	
+	private String getMD5(String string) {
+		try {
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			md.update(string.getBytes());
+		    String hashedString = DatatypeConverter
+		    	      .printHexBinary(md.digest()).toUpperCase();
+		    return hashedString;
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
 		}
 	}
 }
