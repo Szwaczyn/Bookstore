810d2e980c244a03b999874494b5804d7b7146c8
ms <ms@WAWL2011.waw.sita.main>
2020-03-16 14:36:43 +0100

    security

--- src/bookstore/utils/FliterLogged.java
+++ src/bookstore/utils/FliterLogged.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -18,23 +19,10 @@ import bookstore.entity.User;
 @WebFilter("/*")
 public class FliterLogged implements Filter {
 
-    /**
-     * Default constructor. 
-     */
-    public FliterLogged() {
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see Filter#destroy()
-	 */
 	public void destroy() {
 		// TODO Auto-generated method stub
 	}
 
-	/**
-	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
-	 */
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 		HttpServletRequest req = (HttpServletRequest) request;
 		HttpServletResponse resp = (HttpServletResponse) response;
@@ -51,9 +39,6 @@ public class FliterLogged implements Filter {
 		chain.doFilter(request, response);
 	}
 
-	/**
-	 * @see Filter#init(FilterConfig)
-	 */
 	public void init(FilterConfig fConfig) throws ServletException {
 		// TODO Auto-generated method stub
 	}
